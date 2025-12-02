97e8dfaa9cbdddde2b00fa95efb4a03041487689
manen <49951816+manen@users.noreply.github.com>
2025-12-02 11:18:10 +0100

    basic http fs server

---
+++ kserve/src/dir.rs
@@ -0,0 +1,56 @@
+use std::path::{Component, Path, PathBuf};
+
+use anyhow::Context;
+
+fn normalize_without_escape(user: &Path) -> Option<PathBuf> {
+	let mut stack = Vec::new();
+
+	for comp in user.components() {
+		match comp {
+			Component::ParentDir => {
+				if stack.is_empty() {
+					// would escape
+					return None;
+				}
+				stack.pop();
+			}
+			Component::Normal(s) => stack.push(s.to_owned()),
+			Component::CurDir => {}
+			Component::RootDir | Component::Prefix(_) => return None,
+		}
+	}
+
+	Some(stack.iter().collect())
+}
+
+fn join_without_escape(a: &Path, untrusted_b: &Path) -> anyhow::Result<PathBuf> {
+	let b = normalize_without_escape(untrusted_b);
+	let b = b.with_context(|| {
+		format!(
+			"failed to normalize {} without it leaking into the outer fs",
+			untrusted_b.display()
+		)
+	})?;
+
+	Ok(a.join(b))
+}
+
+#[derive(Clone, Debug)]
+pub struct Dir {
+	path: PathBuf,
+}
+impl Dir {
+	pub fn new(path: PathBuf) -> Self {
+		Self { path }
+	}
+
+	pub async fn md_to_html(&self, path: &Path) -> anyhow::Result<String> {
+		let joined = join_without_escape(&self.path, path)?;
+
+		let s = tokio::fs::read_to_string(&joined)
+			.await
+			.with_context(|| format!("while reading {}", path.display()))?;
+
+		Ok(s)
+	}
+}
--- kserve/src/main.rs
+++ kserve/src/main.rs
@@ -1,5 +1,11 @@
-use actix_web::{App, HttpServer, Responder, get, web};
+use std::path::PathBuf;
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use anyhow::Context;
 use tokio::sync::Mutex;
+
+pub mod dir;
+pub use dir::Dir;
 
 const BIND: (&str, u16) = const {
 	let port = if cfg!(debug_assertions) { 9090 } else { 9090 };
@@ -11,11 +17,42 @@ async fn index() -> impl Responder {
 	"hello index"
 }
 
+async fn root_fallback(path: web::Path<String>, dir: web::Data<Dir>) -> HttpResponse {
+	let path = path.into_inner();
+	let path = PathBuf::from(path);
+	let res = dir.md_to_html(&path).await;
+
+	match res {
+		Ok(a) => HttpResponse::Ok().content_type("text/html").body(a),
+		Err(err) => {
+			let err = format!("{err}\n\n{err:#?}");
+			HttpResponse::BadRequest().body(err)
+		}
+	}
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-	let server = HttpServer::new(move || App::new().service(index))
-		.bind(BIND)?
-		.run();
+	let mut args = std::env::args();
+	let _ = args.next();
+
+	let dir_path = args
+		.next()
+		.map(|s| Ok(PathBuf::from(s)))
+		.unwrap_or_else(std::env::current_dir)
+		.with_context(|| "while getting dir path")?;
+
+	let dir = Dir::new(dir_path);
+	let dir = web::Data::new(dir);
+
+	let server = HttpServer::new(move || {
+		App::new()
+			.app_data(dir.clone())
+			.service(index)
+			.route("/{tail:.*}", web::get().to(root_fallback))
+	})
+	.bind(BIND)?
+	.run();
 	println!("listening on http://{}:{}", BIND.0, BIND.1);
 
 	server.await?;
